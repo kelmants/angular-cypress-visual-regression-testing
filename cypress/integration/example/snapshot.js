c4042a4ffb531dae6a4e49fe66ce4bccdd6d7d56
Kelman Celis <kelmancelis@gmail.com>
2021-08-05 01:00:58 -0300

    fix: has been replaced npm to yarn

--- cypress/integration/example/snapshot.js
+++ cypress/integration/example/snapshot.js
@@ -3,6 +3,7 @@ describe("Home screenshot", () => {
     cy.visit("http://localhost:4200/");
   });
   it("should take a screenshot of the home page", () => {
+    cy.viewport("macbook-16");
     cy.matchImageSnapshot("app");
   });
 });
