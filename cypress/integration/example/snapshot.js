8ae0af644393ba638586a5416d71f4d30057a083
Kelman Celis <kelmancelis@gmail.com>
2021-08-05 01:16:48 -0300

    fix: add size in viewport

--- cypress/integration/example/snapshot.js
+++ cypress/integration/example/snapshot.js
@@ -1,9 +1,9 @@
 describe("Home screenshot", () => {
   beforeEach(() => {
     cy.visit("http://localhost:4200/");
+    cy.viewport(1280, 829);
   });
   it("should take a screenshot of the home page", () => {
-    cy.viewport("macbook-16");
     cy.matchImageSnapshot("app");
   });
 });
