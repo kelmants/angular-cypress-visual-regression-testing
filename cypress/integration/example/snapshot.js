a8680918b82d12d29628af540bdbd02ef9ac8da5
Kelman Celis <kelmancelis@gmail.com>
2021-08-05 01:50:12 -0300

    fix: has been added parametrer to snap

--- cypress/integration/example/snapshot.js
+++ cypress/integration/example/snapshot.js
@@ -1,7 +1,6 @@
 describe("Home screenshot", () => {
   beforeEach(() => {
     cy.visit("http://localhost:4200/");
-    cy.viewport(1280, 829);
   });
   it("should take a screenshot of the home page", () => {
     cy.matchImageSnapshot("app");
